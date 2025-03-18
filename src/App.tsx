4949192e026fbd812fa8d903bf65162e032cda2d
dc-choi <ddagae0805@gmail.com>
2025-03-18 14:59:50 +0900

    tailwind로 교체

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
 import { data } from '@/data/data';
-import {
-  Container,
-  Left,
-  ChangedP,
-  GptH2,
-  GptH2Circle,
-  Right,
-  StartedP,
-  Button,
-  ChangedDiv,
-  ButtonDiv,
-} from '@/style/App.Style';
 import { IApp } from './@types/app';
 
 /**
@@ -45,23 +33,40 @@ const App = ({ name, name2, startName }: IApp) => {
   };
 
   return (
-    <Container>
-      <Left>
-        <GptH2>
-          ChatGPT <GptH2Circle />
-        </GptH2>
-        <ChangedDiv>
-          <ChangedP>{text}</ChangedP>
-        </ChangedDiv>
-      </Left>
-      <Right>
-        <StartedP>{startName === `Get started` ? startName : `is Not ${startName}`}</StartedP>
-        <ButtonDiv>
-          <Button onClick={() => alertFuc(name)}>{name}</Button>
-          <Button onClick={() => alertFuc(name2)}>{name2}</Button>
-        </ButtonDiv>
-      </Right>
-    </Container>
+    <div className="flex h-screen">
+      {/* 왼쪽 섹션 */}
+      <div className="flex flex-col flex-[60%] bg-[#210b61] p-5">
+        <h2 className="text-[#d292ff] text-2xl mb-5 ml-5 flex items-center">
+          ChatGPT
+          <span className="w-5 h-5 bg-[#d292ff] rounded-full inline-block ml-2"></span>
+        </h2>
+
+        <div className="mt-[33%] ml-8 flex items-center">
+          <p className="text-[#d292ff] text-4xl font-bold whitespace-pre-wrap">{text}</p>
+        </div>
+      </div>
+
+      {/* 오른쪽 섹션 */}
+      <div className="flex flex-col flex-[40%] bg-black justify-center items-center">
+        <p className="text-white text-3xl font-bold">
+          {startName === 'Get started' ? startName : `is Not ${startName}`}
+        </p>
+        <div className="flex mt-4">
+          <button
+            className="px-6 py-2 bg-blue-500 text-white rounded-md hover:text-black mr-2"
+            onClick={() => alertFuc(name)}
+          >
+            {name}
+          </button>
+          <button
+            className="px-6 py-2 bg-blue-500 text-white rounded-md hover:text-black"
+            onClick={() => alertFuc(name2)}
+          >
+            {name2}
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createGlobalStyle } from 'styled-components';
 import App from './App.tsx';
-
-export const GlobalStyle = createGlobalStyle`
-body {
-    margin: 0;
-    padding: 0;
-}
-`;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle />
     <App name="Log In" name2="Sign up" startName="Get started" />
   </React.StrictMode>,
 );
