c8ec9abff236adfd330a4a6c65722a556beae1c7
dc-choi <ddagae0805@gmail.com>
2025-03-18 15:24:56 +0900

    github pages 배포

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/react_study',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
