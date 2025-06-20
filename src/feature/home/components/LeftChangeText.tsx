dc20cc24cb26037da56d97976b875d8a74ffc7b1
dc-choi <ddagae0805@gmail.com>
2025-06-20 14:43:17 +0900

    fix: 리렌더링 최소화

---
+++ src/feature/home/components/LeftChangeText.tsx
@@ -0,0 +1,30 @@
+import { P } from '@/shared/components/P.tsx';
+import { useEffect, useRef, useState } from 'react';
+import { mock } from '@/feature/home/api/mock.ts';
+
+export const LeftChangeText = () => {
+  const [count, setCount] = useState<number>(0);
+  const [text, setText] = useState<string>('');
+
+  const handleText = () => {
+    if (count < mock.length - 1) setCount(count + 1);
+    else setCount(0);
+
+    setText(mock[count]);
+  };
+
+  const timeRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(() => {
+    timeRef.current = setInterval(handleText, 2000);
+
+    return () => {
+      if (timeRef.current) {
+        clearInterval(timeRef.current);
+        timeRef.current = null;
+      }
+    };
+  }, [count]);
+
+  return <P style={'text-[#d292ff] text-4xl font-bold whitespace-pre-wrap'} text={text} />;
+};
