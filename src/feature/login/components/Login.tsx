f7f5527acafa4d031d8f03591195b16da7326534
dc-choi <ddagae0805@gmail.com>
2025-06-23 17:28:25 +0900

    feat: react hook form 사용해보기

--- src/feature/login/components/Login.tsx
+++ src/feature/login/components/Login.tsx
@@ -1,7 +1,27 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from '@/shared/components/Card.tsx';
+
 export const Login = () => {
   return (
-    <>
-      <p>아직 아무것도 개발 X</p>
-    </>
+    <div className={'flex size-full justify-center items-center'}>
+      <Card className="bg-black text-white bg-center size-1/2">
+        <CardHeader>
+          {/* <CardTitle>로그인</CardTitle> */}
+          <CardDescription>계정과 패스워드를 입력해주세요.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <CardAction>로그인</CardAction>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
